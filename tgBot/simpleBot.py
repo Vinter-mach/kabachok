bfe079cc0847d8176d94fa2afe2b831b97612f58
Ovchinnikov Danila <danovch24@gmail.com>
2025-03-10 15:47:34 +0100

    simple bot 1

---
+++ tgBot/simpleBot.py
@@ -0,0 +1,26 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import Message
+from aiogram.filters import Command
+
+TOKEN = "ТОКЕН в ТГ"
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+
+@dp.message(Command("start"))
+async def start_command(message: Message):
+    await message.answer("Сосал?")
+
+
+@dp.message(Command("help"))
+async def help_command(message: Message):
+    await message.answer("\n/start - что бы стать крутым")
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
