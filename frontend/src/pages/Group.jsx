48cd9f311a858881c4a9a0c1e735942c733ebee0
Vinter-mach <vintermach@gmail.com>
2025-04-09 14:46:30 +0500

    merge + navigate

---
+++ frontend/src/pages/Group.jsx
@@ -0,0 +1,39 @@
+import {useState} from 'react';
+import '../styles/Group.css';
+import { useNavigate } from 'react-router-dom';
+
+function Group() {
+    const [inputValue, setInputValue] = useState('');
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate('/test');
+    };
+
+    const isButtonDisabled = inputValue.trim() === '';
+
+    return (
+        <div className="background-wrapper"> {/* Новая обёртка */}
+            <div className="container">
+                <h2 className="title">добавить группу</h2>
+                <input
+                    type="text"
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                    placeholder="+"
+                    className="input-field"
+                />
+                <button
+                    onClick={handleClick}
+                    className="button"
+                    disabled={isButtonDisabled}
+                >
+                    добавить
+                </button>
+            </div>
+        </div>
+    );
+}
+
+
+export default Group;
---
+++ frontend/src/pages/Test.jsx
@@ -0,0 +1,15 @@
+import {useState} from 'react';
+import '../styles/Group.css';
+
+function Test() {
+    return (
+        <div className="background-wrapper"> {/* Новая обёртка */}
+            <div className="container">
+                <h2 className="title">Сосал?</h2>
+            </div>
+        </div>
+    );
+}
+
+
+export default Test;
