ec2c4bb358eac8fc6213fde80a14c9b5ed709f06
mzukov <Mikhail.Ziukov@urfu.me>
2025-04-14 11:49:51 +0500

    Extra services deleted

--- Cs_backend/Cs_backend/Cs_backend/Services/TasksGetter.cs
+++
@@ -1,9 +0,0 @@
-namespace Cs_backend.Services;
-
-public class TasksGetter
-{
-    public ICollection<(Guid TaskId, string TaskName, int Number)> GetTasks(Guid courseId)
-    {
-        throw new NotImplementedException();
-    }
-}
