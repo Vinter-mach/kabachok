b665204d2dd728bfed55f2c564424e510a622158
flexihtala <flexihtala@gmail.com>
2025-04-19 00:27:24 +0500

    фикс автообновления

--- frontend/src/pages/Course.jsx
+++ frontend/src/pages/Course.jsx
@@ -1,5 +1,5 @@
-import {useState} from 'react';
-import {useNavigate} from 'react-router-dom';
+import { useState, useEffect, useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function SelectionPage() {
     const navigate = useNavigate();
@@ -8,19 +8,69 @@ function SelectionPage() {
     const [newOption, setNewOption] = useState('');
     //старые опции
     const [options, setOptions] = useState([]);
+    const token = localStorage.getItem('token');
+
+    const fetchCourses = useCallback(async () => {
+        try {
+            const response = await fetch('http://localhost:5249/courses/', {
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                },
+            });
+            if (!response.ok) {
+                throw new Error('Ошибка при получении курсов');
+            }
+            const data = await response.json();
+            setOptions(data);
+            console.log('Курсы:', data);
+        } catch (error) {
+            console.error('Ошибка:', error);
+        }
+    }, [token]);
+
+    useEffect(() => {
+        if (token) {
+          fetchCourses();
+        }
+      }, [fetchCourses, token]);
 
     // Добавление нового варианта
-    const handleAddOption = () => {
-        if (newOption.trim() && !options.includes(newOption)) {
-            setOptions([...options, newOption]);
+    // const handleAddOption = () => {
+    //     if (newOption.trim() && !options.includes(newOption)) {
+    //         setOptions([...options, newOption]);
+    //         setNewOption('');
+    //     }
+    // };
+
+    const handleAddOption = async () => {
+        try {
+            const response = await fetch('http://localhost:5249/courses/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                },
+                body: JSON.stringify({ name: newOption }),
+            });
+
+            if (!response.ok) {
+                throw new Error('Ошибка при добавлении группы');
+            }
+
+            const newCourse = await response.json();
+            setOptions([...options, newCourse]);
             setNewOption('');
+            fetchCourses();
+            console.log('Курс добавлен:', newCourse);
+        } catch (error) {
+            console.error('Ошибка:', error);
         }
     };
 
     // Переход на следующую страницу
     const handleNavigate = () => {
         if (selectedOption) {
-            navigate('/group', {state: {selectedOption}});
+            navigate('/group', { state: { selectedOption } });
         }
     };
 
@@ -38,8 +88,8 @@ function SelectionPage() {
                 >
                     <option value="">тыкни на меня</option>
                     {options.map((option, index) => (
-                        <option key={index} value={option}>
-                            {option}
+                        <option key={index} value={option.name}>
+                            {option.name}
                         </option>
                     ))}
                 </select>
