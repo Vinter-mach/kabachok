2334ca2518ca1d44c3f6e65d010f8ceaae64311a
mzukov <Mikhail.Ziukov@urfu.me>
2025-04-25 18:15:19 +0500

    StatusId fixed

--- Cs_backend/Cs_backend/Cs_backend/Repositories/HomeworkRepository.cs
+++ Cs_backend/Cs_backend/Cs_backend/Repositories/HomeworkRepository.cs
@@ -21,7 +21,7 @@ public class HomeworkRepository(ApplicationContext applicationContext)
 
         existing.Comment = entity.Comment;
         existing.Grade = entity.Grade;
-        entity.Status = entity.Status;
+        entity.StatusId = entity.StatusId;
 
         await applicationContext.SaveChangesAsync();
     }
